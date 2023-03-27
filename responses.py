01f429327d37c909b2b775dc3c65cc99c187b8b2
BrandonC3207 <111989490+BrandonC3207@users.noreply.github.com>
2023-03-27 13:33:19 -0400

    Added Files from Previous Proof of Concept
    Token was removed as this is a public repo, before running ensure to create a token using developer tools, and when pushing changes make sure to not include the token as discord will change the token.

---
+++ responses.py
@@ -0,0 +1,11 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'hello world':
+        return 'Hello human'
+
+    if p_message == 'shop':
+        return 'This feature has not been implemented. However here is a mock shop with no images because the moderator is lazy\nOni Vandal\nSmite Knife\nGaleria Bucky\nGlitchop Dagger'
+
+    if p_message == 'help':
+        return 'Currently only hello world, shop and help are the only responding messages'
