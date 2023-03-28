43c0368341eb7d5779260e3fc45acf111d91450c
BrandonC3207 <111989490+BrandonC3207@users.noreply.github.com>
2023-03-28 13:01:06 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTA5MDAyODQzNjIxMjQzMjk3OA.GuPBE_.N6t47qOIhHWMswkErjkRxWLvzlADuACigwTlHk'
+    TOKEN = ''
     intents = discord.Intents.default()
     intents.message_content = True 
     client = discord.Client(intents=intents)
