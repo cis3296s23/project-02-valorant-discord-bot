87cc0128c2c51c56d8f460f6a699945a68c8fc24
aTran47 <tuk82621@temple.edu>
2023-04-02 19:48:46 -0400

    Basic Agent Function added
    Basic agent descriptions pulled from Valorant-API added. List function needs to be implemented. In general, needs to be more robust.

--- cogs/agents.py
+++ cogs/agents.py
@@ -10,10 +10,22 @@ class agents(commands.Cog):
     async def agent(self, ctx, arg):
         r = requests.get("https://valorant-api.com/v1/agents")
         print(r.status_code)
-        print(r.text)
-        for entry in r.json()['data']:
-            if entry['displayName'] == arg:
-                await ctx.send(entry['uuid'])
+        print(arg)
+        if arg == 'list':
+            await ctx.send("List not yet implemented")
+        else:
+            for entry in r.json()['data']:
+                if entry['displayName'] == arg:
+                    uuid = entry['uuid']
+                    displayIcon = entry['displayIconSmall']
+                    description = entry['description']
+
+                    break
+            embed = discord.Embed(title=arg, description = description, colour = discord.Colour.random())
+            embed.set_thumbnail(url=displayIcon)
+            await ctx.send(embed=embed)
+                #await ctx.send(entry['uuid'])
+        
 
 async def setup(client):
     await client.add_cog(agents(client))
