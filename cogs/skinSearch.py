ce8409f623f040c6fdeb6716e20afba5c5ed13a3
aTran47 <tuk82621@temple.edu>
2023-04-15 17:46:04 -0400

    Fixed skinSearch
    skinSearch only worked on skins that were two words long. Fixed such that regardless of if the collection or the weapon name has a space it will work

--- cogs/skinSearch.py
+++ cogs/skinSearch.py
@@ -7,20 +7,21 @@ class skinSearch(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def skin(self, ctx, arg1, arg2):
+    async def skin(self, ctx, *arg):
             r = requests.get("https://valorant-api.com/v1/weapons/skins")
             print(r.status_code)
-            print(arg1)
-            print(arg2)
-            arg1 += " "
-            arg1 += arg2
-            print(arg1)
+            print(arg)
+            #print(arg2)
+            #arg1 += " "
+            #arg1 += arg2
+            str = ' '.join(arg)
+            print(str)
             for entry in r.json()['data']:
-                if entry['displayName'] == arg1:
+                if entry['displayName'] == str:
                     #await ctx.send("The skin has been found")
                     displayIcon = entry['displayIcon']
                     break
-            embed = discord.Embed(title=arg1, colour = discord.Colour.random())
+            embed = discord.Embed(title=str, colour = discord.Colour.random())
             embed.set_image(url=displayIcon)
             await ctx.send(embed=embed)
             #await ctx.send(displayIcon)
