2f576f69948a30c84abea75f93234735159dd894
Christian Smith <tuk96703@temple.edu>
2023-04-16 19:45:01 -0400

    Working version of photo printing per user rank information and discord embedding, needs improvement!

--- cogs/rank.py
+++ cogs/rank.py
@@ -1,5 +1,6 @@
 import requests
 import discord
+import os
 from discord.ext import commands
 
 class rankCommand(commands.Cog):
@@ -30,65 +31,73 @@ class rankCommand(commands.Cog):
         rankDataList = rank.split() #split on a space character to get rank name and number
         rankName = rankDataList[0] 
         rankNumber = rankDataList[1]
-        rankFile = "" #this variable will be used for a check 1-3 on what rank they are in the category (we will combine all neccessary information later into this string to be read for a scope check)
+
+        #this variable will be used for a check 1-3 on what rank they are in the category (we will combine all neccessary information later into this string to be read for a scope check)
+        currentWorkingDirectory = os.getcwd() #get the current working directory
+        rankFile = currentWorkingDirectory + "/images/ranks/" + rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search (in the given directory)
+        noRankBugFlag = 0 #if this is zero it means we never hit a rank meaning a bug is present
 
         #   RR is formatted by everything being together so we do not need to format 
         #   it we can just iterate through it till we hit the first 'R' in "RR" to get the number we want
 
         if (rankName == "Iron"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Iron")
             embed = discord.Embed(title="Iron")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Bronze"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Bronze")
             embed = discord.Embed(title="Bronze")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Silver"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Silver")
             embed = discord.Embed(title="Silver")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Gold"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Gold")
             embed = discord.Embed(title="Gold")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Platinum"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Platinum")
             embed = discord.Embed(title="Platinum")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Diamond"):
-            print("users rank = iron")
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Diamond")
             embed = discord.Embed(title="Diamond")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Ascendant"):
-            print("users rank = iron")   
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Ascendant")   
             embed = discord.Embed(title="Ascendant")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Immortal"):
-            print("users rank = iron") 
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Immortal") 
             embed = discord.Embed(title="Immortal")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
         if (rankName == "Radiant"):
-            print("users rank = iron") 
-            rankFile = rankName + "_" + rankNumber + "_Rank.png" #formatting for the file search
+            noRankBugFlag = 1
+            print("users rank = Radiant") 
             embed = discord.Embed(title="Radiant")
             await ctx.send("You rank is: " + rankName + " " + rankNumber, file = discord.File(rankFile) , embed = embed)
 
+        #scope check for if we do not execute any of the above if statements regarding the users rank and the image displayed
+        if (noRankBugFlag == 0):
+            print("ISSUE!!: No rank found for user")
+            await ctx.send("We could not find a rank to display based on your rank")
 
         #TODO
         #what ima do, I am going to seperate the data and have a rank string and a RR string
