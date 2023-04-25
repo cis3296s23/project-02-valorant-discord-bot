771b6939252b93c6b5536af05d8d90326afb97d9
Brandon <tuk16746@temple.edu>
2023-04-25 00:48:42 -0400

    stat command works in full, however aim command is broken and previous match command will not work with beautful soup

--- cogs/stats.py
+++ cogs/stats.py
@@ -11,7 +11,7 @@ class stats(commands.Cog):
     async def aim(self, ctx, riotName:str,riotTag:str):
         #Formats input from user and creates url with info
         url = ("https://blitz.gg/valorant/profile/"+riotName+"-"+riotTag)
-        #print(url)
+        print('Here')
         r = requests.get(url)
         if(r.statuscode == 404):
             print(riotName+riotTag+ "is not a valid input")
@@ -25,6 +25,7 @@ class stats(commands.Cog):
 
         #Formats the above into an embed to be printed on Discord
         embed = discord.Embed(title=riotName+"'s Aim Stats", description="Over the last 20 Matches...", colour = discord.Colour.blue())
+        #embed.set_thumbnail(url='https://cdn-icons-png.flaticon.com/512/1692/1692908.png')
         embed.add_field(name= "Head Shots:", value = headShots, inline = False)
         embed.add_field(name= "Body Shots:", value = bodyShots, inline = False)
         embed.add_field(name= "Leg Shots:", value = legShots, inline = False)
@@ -37,10 +38,13 @@ class stats(commands.Cog):
         r = requests.get(URL)
         #Get content of webpage 
         soup = BeautifulSoup(r.content, 'lxml')
-        #Filter HTML Tree to find
+        #Filter HTML Tree to find for stats
         table = soup.find('div', attrs = {'class':'giant-stats'}) 
         value = table.findAll('span', attrs = {'class':'value'})
-        
+        #Get users image
+        table2 = soup.find('div', attrs = {'class':'ph__container'}) 
+        image = table2.find('img', attrs = {'class':'user-avatar__image'})
+
         #Create array for values
         arr = []
         #loop to find all values in value header
@@ -49,21 +53,23 @@ class stats(commands.Cog):
             arr.append(values.string)
              
         embed = discord.Embed(title='Current Competitive Season Stats', description = 'Stats of player: '+riotName, colour = discord.Colour.random())
-        embed.add_field(name= "Damage/Round: ", value = arr[1], inline = False)
+        embed.set_thumbnail(url='https://img.icons8.com/color/512/valorant.png')
+        embed.add_field(name= "Damage/Round: ", value = arr[0], inline = False)
         embed.add_field(name= "K/D Ratio: ", value = arr[1], inline = False)
         embed.add_field(name= "Headshot%: ", value = arr[2], inline = False)
         embed.add_field(name= "WinRate: ", value = arr[3], inline = False)
         await ctx.send(embed=embed)
 
+    #This command does not work
     @commands.command()
     async def matches(self, ctx, riotName:str, riotTag:str):
-        URL = ("https://blitz.gg/valorant/profile/"+riotName+"-"+riotTag)
+        URL = "https://tracker.gg/valorant/profile/riot/"+ riotName + "%23"+ riotTag
         #Connects to webpage
         r = requests.get(URL)
         #Get content of webpage 
         soup = BeautifulSoup(r.content, 'lxml')
         #Filter HTML Tree to find
-        table = soup.find('div', attrs = {'class':'⚡45493bd1 match-list'}) 
-        value = table.findAll('span', attrs = {'class':'⚡57078c65'})
+        table = soup.find('div', attrs = {'class':'trn-gamereport-list__group-entries'}) 
+        value = table.find('div', attrs = {'vmr-stats trn-match-row__section trn-match-row__section--right gap-4'})
 async def setup(client):
     await client.add_cog(stats(client))
